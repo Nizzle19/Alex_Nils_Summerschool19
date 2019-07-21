e028ba71a1173f5ec10b641b59fc619bdff5ef4f
ankochnev <52839101+ankochnev@users.noreply.github.com>
2019-07-21 20:21:19 +0200

    Add files via upload

---
+++ HW02/output.h
@@ -0,0 +1,9 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+// Outputs the integer i to stdout with a trailing space
+void outputT2(int i);
+
+// Outputs each element of an array of size count on its own line
+void outputT5(int* arr, unsigned int count);
+
+#endif
